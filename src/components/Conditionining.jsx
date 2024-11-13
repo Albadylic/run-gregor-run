dd517d6efc5ad83fd844cdfdb69f63c99be15259
Gregor <43413852+Albadylic@users.noreply.github.com>
2024-11-13 15:04:53 +0000

    Render specific routines on conditioning days and add navigation

--- src/components/Conditionining.jsx
+++ src/components/Conditionining.jsx
@@ -1,20 +1,78 @@
+import { useState } from "react";
 import conditioningData from "../data/conditioning.json";
 
-export default function Conditioning() {
+function renderDay(dayName, id) {
+  const day = conditioningData["days"][id];
   return (
     <div>
-      {conditioningData["plan"]["days"].map((day) => (
-        <div>
-          <h2>{day.name}</h2>
-          {day.exercises.map((exercise) => (
-            <div>
-              <h3>{exercise.name}</h3>
-              <p>Sets: {exercise.sets}</p>
-              <p>Reps: {exercise.reps}</p>
-            </div>
-          ))}
+      <h2>{`${dayName}: ${day.name}`}</h2>
+      {day.exercises.map((exercise) => (
+        <div key={exercise.name}>
+          <h3>{exercise.name}</h3>
+          <p>Sets: {exercise.sets}</p>
+          <p>Reps: {exercise.reps["1"]}</p>
         </div>
       ))}
     </div>
   );
+}
+
+export default function Conditioning() {
+  const dayIndex = new Date().getDay();
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const today = days[dayIndex];
+
+  const [dayState, setDayState] = useState(today);
+  const daysToPlan = {
+    Monday: "0",
+    Wednesday: "1",
+    Friday: "0",
+  };
+
+  const DayNav = () => {
+    return (
+      <nav>
+        <ul>
+          <li key="Monday" onClick={() => setDayState("Monday")}>
+            Monday
+          </li>
+          <li key="Wednesday" onClick={() => setDayState("Wednesday")}>
+            Wednesday
+          </li>
+          <li key="Friday" onClick={() => setDayState("Friday")}>
+            Friday
+          </li>
+          <li key="Today" onClick={() => setDayState(today)}>
+            Today
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+
+  if (daysToPlan.hasOwnProperty(dayState)) {
+    return (
+      <>
+        <DayNav />
+        {renderDay(dayState, daysToPlan[dayState])}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <DayNav />
+        <div>
+          <h2>No training planned for {dayState}</h2>
+        </div>
+      </>
+    );
+  }
 }
