cc597c037b49f1ea43b2bb944d4c528d17605426
Gregor <43413852+Albadylic@users.noreply.github.com>
2024-11-17 11:46:48 +0000

    Rebuilding front page

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import Conditioning from "./Conditionining";
 import Running from "./Running";
+import months from "../data/months.json";
+import { Link } from "react-router-dom";
 
 export default function Today() {
   const dayIndex = new Date().getDay();
+  const monthIndex = new Date().getMonth();
+
   const days = [
     "Sunday",
     "Monday",
@@ -15,6 +19,26 @@ export default function Today() {
   ];
   const today = days[dayIndex];
 
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const thisMonth = monthNames[monthIndex];
+
+  const currentMonthData = months["months"].filter(
+    (obj) => obj.name === thisMonth
+  )[0];
+
   const daysToPlan = {
     Monday: "conditioning",
     Tuesday: "running",
@@ -24,18 +48,58 @@ export default function Today() {
   };
 
   const [dayState, setDayState] = useState(today);
+  const [monthState, setMonthState] = useState(currentMonthData);
 
-  if (daysToPlan[dayState] === "conditioning") {
-    return <Conditioning />;
-  } else if (daysToPlan[dayState] === "running") {
-    return <Running />;
-  } else {
+  const Month = () => {
     return (
-      <>
-        <div>
-          <h2>No training planned for {dayState}</h2>
-        </div>
-      </>
+      <div>
+        <h2>
+          {monthState.name}: {monthState.title}
+        </h2>
+        <p>{monthState.focus}</p>
+        <p>Goal: {monthState.goal}</p>
+        <p>Mantra: {monthState.mantra}</p>
+      </div>
     );
-  }
+  };
+
+  const MonthNav = () => {
+    return (
+      <nav className="Sidebar">
+        <ul>
+          <li
+            key={currentMonthData.name}
+            onClick={() => setMonthState(currentMonthData)}
+          >
+            This month
+          </li>
+          {months["months"].map((month) => (
+            <li key={month.name} onClick={() => setMonthState(month)}>
+              {month.name}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  };
+
+  return (
+    <div className="page_container">
+      <MonthNav />
+      <div className="page_body">
+        <Month />
+        {daysToPlan[dayState] === "conditioning" ? (
+          <h2>
+            Today is focused on <Link to="/conditioning">conditioning</Link>
+          </h2>
+        ) : daysToPlan[dayState] === "running" ? (
+          <h2>
+            Today is focused on <Link to="/running">running</Link>
+          </h2>
+        ) : (
+          <h2>No training planned for {dayState}</h2>
+        )}
+      </div>
+    </div>
+  );
 }
